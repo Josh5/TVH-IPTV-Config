6098f1bda0dc35187560691944acc67728a07856
Josh.5 <jsunnex@gmail.com>
2024-06-07 00:28:01 +1200

    Add a HLS proxy
    In my setup I am using a VPN. However there are a few streams that I will not want to go through this VPN. For these, I will setup a separate docker network that does not use the VPN tunnel interface and will use this HLS proxy to access those streams.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@
 # File Created: Monday, 13th May 2024 4:20:35 pm
 # Author: Josh.5 (jsunnex@gmail.com)
 # -----
-# Last Modified: Monday, 13th May 2024 4:24:23 pm
+# Last Modified: Friday, 7th June 2024 12:24:22 am
 # Modified By: Josh.5 (jsunnex@gmail.com)
 ###
 
@@ -19,6 +19,12 @@ print_log() {
   local message="${@:2}"
   echo "[${timestamp}] [${pid}] [${level^^}] ${message}"
 }
+
+# Catch term signal and terminate any child processes
+_term() {
+    kill -TERM "$proxy_pid" 2>/dev/null
+}
+trap _term SIGTERM SIGINT
 
 # Ensure the customer is set
 print_log info "APP_HOST_IP: ${APP_HOST_IP:-APP_HOST_IP variable has not been set}"
@@ -36,11 +42,11 @@ if [ "X$@" != "X" ]; then
 else
     # Install packages (if requested)
     if [ "${RUN_PIP_INSTALL}" = "true" ]; then
-        python3 -m venv --symlinks --clear /app/venv-docker
-        source /app/venv-docker/bin/activate
+        python3 -m venv --symlinks --clear /var/venv-docker
+        source /var/venv-docker/bin/activate
         python3 -m pip install --no-cache-dir -r /app/requirements.txt
     else
-        source /app/venv-docker/bin/activate
+        source /var/venv-docker/bin/activate
     fi
 
     # Execute migrations
@@ -48,6 +54,10 @@ else
         print_log info "Running TVH-IPTV-Config DB migrations"
         flask db upgrade
     fi
+
+    # Run proxy
+    gunicorn -w 2 --threads 4 -b 0.0.0.0:9987 proxy:app &
+    proxy_pid=$!
 
     # Run Flask server
     flask run
