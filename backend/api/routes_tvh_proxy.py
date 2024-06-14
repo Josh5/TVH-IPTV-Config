9fecb76a321f357ec90663db5f2ba6752b5ba9fd
Josh.5 <jsunnex@gmail.com>
2024-06-14 14:47:02 +1200

    Get tuner count from playlist config for HDHomeRun emulation

--- backend/api/routes_tvh_proxy.py
+++ backend/api/routes_tvh_proxy.py
@@ -3,9 +3,6 @@
 from backend.api import blueprint
 from flask import jsonify, current_app, render_template_string, Response
 
-from backend.channels import read_config_all_channels
-from backend.epgs import generate_epg_channel_id
-
 device_xml_template = """<?xml version="1.0" encoding="UTF-8"?>
 <root xmlns="urn:schemas-upnp-org:device-1-0">
     <specVersion>
@@ -56,6 +53,7 @@ def _get_tvh_settings():
 
 def _get_channels(playlist_id):
     return_channels = []
+    from backend.channels import read_config_all_channels
     channels_config = read_config_all_channels(filter_playlist_ids=[int(playlist_id)])
     for channel in channels_config:
         if channel['enabled']:
@@ -63,10 +61,18 @@ def _get_channels(playlist_id):
     return return_channels
 
 
+def _get_playlist_connection_count(playlist_id):
+    from backend.playlists import read_config_one_playlist
+    playlist_config = read_config_one_playlist(playlist_id)
+    return playlist_config.get('connections', 1)
+
+
 def _get_discover_data(playlist_id=0):
+    config = current_app.config['APP_CONFIG']
+    settings = config.read_settings()
     tvh_settings = _get_tvh_settings()
     device_name = f'TVH-IPTV-Config-{playlist_id}'
-    tuner_count = 2
+    tuner_count = _get_playlist_connection_count(playlist_id)
     device_id = f'tic-12345678-{playlist_id}'
     device_auth = f'tic-{playlist_id}'
     base_url = f'{tvh_settings["tic_base_url"]}/tic-api/hdhr_device/{playlist_id}'
@@ -88,6 +94,7 @@ def _get_lineup_list(playlist_id):
     use_tvh_source = True
     tvh_settings = _get_tvh_settings()
     lineup_list = []
+    from backend.epgs import generate_epg_channel_id
     for channel_details in _get_channels(playlist_id):
         channel_id = generate_epg_channel_id(channel_details["number"], channel_details["name"])
         # TODO: Add support for fetching a stream from this application without using TVH as a proxy
